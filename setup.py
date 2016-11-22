35464b74cb36748908947db25db197667dfee9fe
ostrokach <ostrokach@gmail.com>
2016-11-22 14:28:17 -0500

    Bump version and upload to PyPi.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='pdbx-mmcif',
     packages=['pdbx', 'pdbx.writer', 'pdbx.reader'],
-    version='0.0.1',
-    description="Parser for structures in the protein data bank (PDB) in the mmCIF format",
-    author="various",
+    version='0.0.2',
+    description="Parser for structures in the protein data bank (PDB) in the mmCIF format.",
+    maintainer="Various",
+    maintainer_email="alex.strokach@utoronto.ca",
     url="https://github.com/ostrokach/pdbx",
     keywords=['protein', 'pdb'],
     classifiers=[
